ecfca6a186b52d3a8000dffa48276f63fc123844
Issei <52270000+Issei0804-ie@users.noreply.github.com>
2023-11-23 18:51:56 +0900

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
